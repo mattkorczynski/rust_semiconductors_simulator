a7886b3e87f0fc97221e12388d89a075c9923199
Matt Korczynski <korczynski.mateusz@gmail.com>
2022-09-17 23:07:18 +0200

    Added server.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
+extern crate iron;
+#[macro_use] extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Serwer dostępny pod adresem localhost:3000...");
+    Iron::new(get_form).http("localhost:3000").unwrap();
 }
+
+fn get_form(_request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(r#"
+        <title> Semiconductors simulator</title>
+        <form action="/sim" method = "post">
+            <input type = "text" name = "n"/>
+            <button type="submit">Simulate</button>
+        </form>
+    "#);
+    Ok(response)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
